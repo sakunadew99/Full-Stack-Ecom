3781729e6eb372618dbe00eeedab015f9f7f549e
Sakuna Dewanarayana <68386969+sakunadew99@users.noreply.github.com>
2024-06-19 20:58:38 +0530

    update styles

--- src/Components/Popular/Popular.jsx
+++ src/Components/Popular/Popular.jsx
@@ -1,23 +1,20 @@
-import React from 'react'
-import './Popular.css'
-import data_products from '../Assets/data'
-import Item from '../Item/Item'
+import React from 'react';
+import './Popular.css';
+import data_products from '../Assets/data';
+import Item from '../Item/Item';
 
 const Popular = () => {
   return (
     <div className='popular'>
-      <h1>Popluar in Women</h1>
+      <h1>Popular in Women</h1>
       <hr />
-        
-      
-        <div className='popular-item'>
-            {data_products.map((item,i) => {
-                return <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price} />
-            })}
-        </div>
+      <div className='popular-items'>
+        {data_products.map((item, i) => {
+          return <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price} />
+})}
       </div>
-    
-  )
+    </div>
+  );
 }
 
-export default Popular
+export default Popular;
